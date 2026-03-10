badbc3d77530cad3485f51b62fd8a5edada9b2da
quoifeur <arthur.hya@gmail.com>
2026-03-10 14:31:10 +0100

    first commit

---
+++ ressources/script/script.js
@@ -0,0 +1,39 @@
+function copyFriendCode(event){
+event.preventDefault();
+const code = "SW-1716-5040-9138"; 
+navigator.clipboard.writeText(code);
+alert("Code ami copié : " + code);
+}
+
+
+
+
+function copyEpicCode(event){
+event.preventDefault();
+const code = "quoifeursan"; 
+navigator.clipboard.writeText(code);
+alert("Pseudo copié : " + code);
+}
+
+
+
+function copyLOLCode(event){
+event.preventDefault();
+const code = "KC Quoifeur#DLipa"; 
+navigator.clipboard.writeText(code);
+alert("Pseudo copié : " + code);
+}
+
+function copyDiscord(event){
+event.preventDefault();
+const code = "quoifeurssb"; 
+navigator.clipboard.writeText(code);
+alert("Pseudo copié : " + code);
+}
+
+function copyMSN(event){
+event.preventDefault();
+const code = "QuoiFeur64@escargot.chat"; 
+navigator.clipboard.writeText(code);
+alert("Adresse copié : " + code);
+}
